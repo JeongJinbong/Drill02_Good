fb1cd626d799b0c0a1adb1162de00c44484e5040
JeongJinbong(NoteBook) <jbong0104@naver.com>
2023-09-20 17:43:26 +0900

    전체 뼈대 완성

--- character_run.py
+++ character_run.py
@@ -1,2 +1,24 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+import math
+from pico2d import *
+
+open_canvas()
+
+grass = load_image('grass.png')
+character= load_image('character.png')
+
+def run_circle():
+    print('CIRCLE')
+    pass
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+
+while True:
+    run_circle()
+    run_rectangle()
+    break
+
+
+
+close_canvas()
